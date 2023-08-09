2a28337686e39af4bb6f5da76d800b1643d3e489
SUJAN <62798625+sujan-coder@users.noreply.github.com>
2023-08-09 17:48:05 +0530

    Create check whether a given number is a 'Perfect' number or not.c
    Example:- 6
    Factors of 6 (except itself) are 1,2,3.
    The Sum of these factors 1+2+3 = 6 So, 6 is a Perfect number.
    
    Another example:- 28
    Factors of 28 (except itself) are 1, 2, 4, 7, 14
    The sum of factors of 28 = 1+2+4+7+14 = 28
    Hence, 28 is also a perfect number.
    
    
    
    O/P:
    enter the number:
    6
    6 is a perfect number.
    
    
    enter the number:
    15
    15 is not a perfect number.

---
+++ check whether a given number is a 'Perfect' number or not.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,n,sum=0;
+    printf("enter the number:\n");
+    scanf("%d",&n);
+    for(i=1;i<=n/2;i++)
+    {
+        if(n%i==0)
+            sum+=i;
+    }
+     if(sum==n)
+         printf("%d is a perfect number.\n",n);
+     else
+         printf("%d is not a perfect number.\n",n);
+
+
+    return 0;
+}
+
+
+

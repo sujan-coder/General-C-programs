3135a06fea2554935441acab007dca1c978cdbea
SUJAN <62798625+sujan-coder@users.noreply.github.com>
2023-08-09 11:20:05 +0530

    Create Factorial of given number.c

---
+++ Factorial of given number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i,n,fact=1;
+    printf("enter the number: \n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        fact=fact*i;
+    }
+    printf("factorial=%d",fact);
+    return 0;
+}
